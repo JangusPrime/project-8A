63d0b63b0d4ecb4a234d0597ea6e05a8295647d0
JangusPrime <jf.angustia@gmail.com>
2024-10-09 10:52:45 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Example</title>
+</head>
+<body>
+
+  <button id="buyTickets">Buy Tickets</button>
+
+  <script>
+    document.querySelector("#buyTickets").addEventListener("click", function() {
+      alert("Tickets will be available soon!");
+    });
+  </script>
+
+</body>
+</html>
